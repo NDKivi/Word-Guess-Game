44349b05839c116f4501ccf4897fc0b42eb3c69e
NDKivi <kivineal@outlook.com>
2018-06-14 19:39:26 -0500

    mostly done

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Word-Guess-Game
 UMN Coding Boot Camp Homework 3 - JavaScript 
+
+## Things to keep track of
+* The secret word to be discovered by guessing letters
+* The number of guesses taken so far or the number of guesses remaining
+* Which letters have been guessed
+* Wins thus far
+* Failures thus far
+
+## States
+* On key press, check whether the letter was guessed
+    * If already guessed, do nothing
+    * If not already guessed
+        * And it is a missing letter or letters of the word, then fill in letters
+        * And it is not a missing letter of the word, then decrease the guesses remaining
+        * Check if the game is over with two scenarios
+            * No guesses remaining: 
+                * show failure screen/play failure sound
+                * increment numLosses
+            * All letters guessed for wrord:
+                * show winner screen/play winner sound
+                * increment numWins
+
+## Input
+* get key input
+* convert key to character for comparison
+
+## Display
+Current game:
+* Current word
+    * Show underscores for unknown letters
+    * Show guessed letters
+* Number of guesses remaining
+    * if time permits, make an animation for it
+
+Totals:
+
+* Number of losses
+* Number of wins
+
+## Technical details
+* Store "mystery word" in String mysteryString
+* Store the string to display in displayString
+* Store failed guesses thus far in String usedLetters
+* Store wins thus far as a number in numWins
+* Store losses thus far in numLosses
+
+## Initialization
+* Set mysteryString to an unused word from the dictionary
+
+## TBD
+* Data structure for dictionary of words?
+* Handle spaces
---
+++ assets/javascript/game.js
@@ -0,0 +1,199 @@
+//===============================================
+// Global variables
+//===============================================
+const ALLOWEDGUESSES = 10;
+let stringBank = [
+    "relativity",
+    "gravity",
+    "parallax",
+    "mercury",
+    "venus",
+    "mars",
+    "jupiter",
+    "saturn",
+    "pluto",
+    "star",
+    "asteroid",
+    "comet",
+    "quasar",
+    "galaxy",
+    "meteor",
+    "nebula",
+    "supaernova",
+    "sunspot",
+    "pulsar",
+    "cepheid",
+    "cosmology",
+    "orbit",
+    "occultation",
+    "penumbra",
+    "vacuum",
+    "accretion",
+    "antimatter",
+    "apastron",
+    "blueshift",
+    "redshift",
+    "retrograde",
+    "spectrometer",
+    "supergiant",
+    "terminator"
+];
+let mysteryString = getNewMysteryString();
+let gameOver = false;
+let pastMysteryStrings = [];
+let lettersNotInMysteryWord = [];
+let allLettersGuessed = [];
+let numWins = 0;
+let numLosses = 0;
+let showArray;
+initializeShowArray();
+
+
+//===============================================
+// Input functions
+//===============================================
+function guessLetter(event) {
+    let y = event.key.toLowerCase();
+    console.log(event.key);
+    if (!gameOver) {
+        if (!isLetter(y)) {
+            document.getElementById("warning").textContent = "Please type a letter";
+        } else if (isAlreadyGuessed(y)) {
+            document.getElementById("warning").textContent = "You already guessed " + y + ". Please pick a new letter.";
+        } else {
+            document.getElementById("warning").textContent = "";
+            updateGame(y);
+        }
+    } else {
+        if (y == " ") {
+            resetGame();
+        }
+    }
+}
+
+function isLetter(myString) {
+    return (myString.length === 1) && myString.match(/[a-z]/i);
+}
+
+function isAlreadyGuessed(myString) {
+    for (let letter of allLettersGuessed) {
+        if (myString === letter) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//===============================================
+// Main logic functions
+//===============================================
+function updateGame(inputLetter) {
+    allLettersGuessed.push(inputLetter);
+    if ((mysteryString.indexOf(inputLetter)) === -1) {
+        lettersNotInMysteryWord.push(inputLetter);
+        if (isGameLost()) {
+            numLosses++;
+            gameOver = true;
+            displayGameOver(false);
+        }
+    } else {
+        for (let i = 0; i < mysteryString.length; i++) {
+            if (mysteryString.charAt(i) === inputLetter) {
+                showArray[i] = true;
+            }
+        }
+        if (isGameWon()) {
+            numWins++;
+            gameOver = true;
+            displayGameOver(true);
+        }
+    }
+    updateDisplay();
+
+}
+
+function isGameLost() {
+    return (lettersNotInMysteryWord.length > ALLOWEDGUESSES);
+}
+
+function isGameWon() {
+    for (let i = 0; i < showArray.length; i++) {
+        if (!showArray[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function resetGame() {
+    pastMysteryStrings.push(mysteryString);
+    mysteryString = getNewMysteryString();
+    initializeShowArray();
+    lettersNotInMysteryWord = [];
+    allLettersGuessed = [];
+    gameOver = false;
+    updateDisplay();
+    document.getElementById("gameovermessage").textContent = "";
+}
+
+function getNewMysteryString() {
+    let index = Math.floor(Math.random() * stringBank.length);
+    return stringBank.splice(index, 1).toString();
+}
+
+function initializeShowArray() {
+    showArray = [];
+    for (let i = 0; i < mysteryString.length; i++) {
+        showArray.push(false);
+    }
+}
+
+//===============================================
+// Output functions
+//===============================================
+function updateDisplay() {
+    displayString();
+    displayLettersNotInMysteryWord();
+    displayLosses();
+    displayWins();
+    displayPastWords();
+}
+
+function displayString() {
+    let str = "";
+    for (let i = 0; i < mysteryString.length; i++) {
+        if (showArray[i]) {
+            str += mysteryString.charAt(i);
+        } else {
+            str += "_";
+        }
+    }
+    document.getElementById("word").textContent = str;
+}
+
+function displayLettersNotInMysteryWord() {
+    document.getElementById("fail").textContent = lettersNotInMysteryWord.toString();
+}
+
+function displayLosses() {
+    document.getElementById("losses").textContent = numLosses;
+}
+
+function displayWins() {
+    document.getElementById("wins").textContent = numWins;
+}
+
+function displayPastWords() {
+    document.getElementById("pastwords").textContent = pastMysteryStrings.toString();
+}
+
+function displayGameOver(isWinner) {
+    let myElement = document.getElementById("gameovermessage");
+    if (isWinner) {
+        myElement.textContent = "You won!!!!!!!!! You can now observe the universe at your leisure with complete control of the Hubble Space Telescope.  Press the space bar to start another round.";
+    } else {
+        myElement.textContent = "You lost!!!!!!!! You passed the event horizon and were obliterated by the singularity of the black hole.  Regardless, press the space bar to start another round.";
+    }
+}
+
+
